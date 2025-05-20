728003f3ca184fad67e7341d1af86899274b1530
Rancorp-max <Rancorp@gmail.com>
2025-05-20 00:04:15 -0400

    Update facetomany.js

--- api/facetomany.js
+++ api/facetomany.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
       {
         input: {
           image,
-          style: "Clay",
+          style: "Pixar",
           prompt: "storybook children's character illustration",
           instant_id_strength: 0.8
         }
