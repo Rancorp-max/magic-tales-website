286eb366774c997ef057ca7da8f583f0e33c47a3
Rancorp-max <Rancorp@gmail.com>
2025-05-20 12:46:28 -0400

    Create poll-avatar.js

---
+++ api/poll-avatar.js
@@ -0,0 +1,11 @@
+// /api/poll-avatar.js
+export default async function handler(req, res) {
+  const id = req.query.id;
+
+  const statusRes = await fetch(`https://api.replicate.com/v1/predictions/${id}`, {
+    headers: { "Authorization": `Token ${process.env.REPLICATE_API_TOKEN}` }
+  });
+
+  const data = await statusRes.json();
+  res.status(200).json(data);
+}
